17b998dcb4dd79ea27d4354dd978da14146a72ef
Ramiro Guzmán C <ramiroguzmanc@gmail.com>
2024-02-21 17:51:49 -0500

    Buscar producto, crear y eliminar

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -7,6 +7,7 @@ import {
   Param,
   Delete,
   Logger,
+  ParseUUIDPipe,
 } from '@nestjs/common'
 import { ProductsService } from './products.service'
 import { CreateProductDto } from './dto/create-product.dto'
@@ -27,17 +28,20 @@ export class ProductsController {
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.productsService.findOne(+id)
+  findOne(@Param('id', ParseUUIDPipe) id: string) {
+    return this.productsService.findOne(id)
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
+  update(
+    @Param('id', ParseUUIDPipe) id: string,
+    @Body() updateProductDto: UpdateProductDto
+  ) {
     return this.productsService.update(+id, updateProductDto)
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.productsService.remove(+id)
+  remove(@Param('id', ParseUUIDPipe) id: string) {
+    return this.productsService.remove(id)
   }
 }
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -3,6 +3,7 @@ import {
   Injectable,
   InternalServerErrorException,
   Logger,
+  NotFoundException,
 } from '@nestjs/common'
 import { CreateProductDto } from './dto/create-product.dto'
 import { UpdateProductDto } from './dto/update-product.dto'
@@ -28,20 +29,41 @@ export class ProductsService {
     }
   }
 
-  findAll() {
-    return `This action returns all products`
+  async findAll() {
+    try {
+      const products = await this.productRepository.find()
+      return products
+    } catch (error) {
+      this.handleDBExceptions(error)
+    }
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} product`
+  async findOne(id: string) {
+    try {
+      const product = await this.productRepository.findOneBy({ id })
+      if (!product) {
+        throw new NotFoundException(
+          'No se ha encontrado el producto solicitado'
+        )
+      }
+      return product
+    } catch (error) {
+      this.handleDBExceptions(error)
+    }
   }
 
   update(id: number, updateProductDto: UpdateProductDto) {
     return `This action updates a #${id} product`
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} product`
+  async remove(id: string) {
+    try {
+      const product = await this.findOne(id)
+      await this.productRepository.remove(product)
+      return 'Producto eliminado exitosamente'
+    } catch (error) {
+      this.handleDBExceptions(error)
+    }
   }
 
   private handleDBExceptions(error: any) {
